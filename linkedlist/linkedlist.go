6ff31705240deba4035166a106816b30af306f31
Mohcine50 <mohcinela1998@gmail.com>
2022-11-11 19:10:07 +0100

    First commit |Linkedlist implemntation

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,50 @@
+package linkedlist
+
+import "fmt"
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+type LinkedList struct {
+	Head *Node
+	Tail *Node
+}
+
+//Linkedlist travers
+
+func (list *LinkedList) Traverse() {
+
+	for list.Head != nil {
+		fmt.Println(list.Head.Value)
+		list.Head = list.Head.Next
+	}
+
+}
+
+// Linked List node Insertion
+/** Insert at the beginnin of the linedlist**/
+func (list *LinkedList) InsertInBeginin(value int) {
+	node := &Node{Value: value, Next: nil}
+	if list.Head == nil {
+		list.Head = node
+	} else {
+		node.Next = list.Head
+		list.Head = node
+	}
+}
+
+/** Insert at the end of the linedlist**/
+func (list *LinkedList) InsertInEnd(value int) {
+	node := &Node{Value: value, Next: nil}
+	if list.Head == nil {
+		list.Head = node
+	} else {
+		var tempNode *Node = list.Head
+		for tempNode.Next != nil {
+			tempNode = tempNode.Next
+		}
+		tempNode.Next = node
+	}
+}
---
+++ stack/stack.go
@@ -0,0 +1,17 @@
+package stack
+
+// stack is linear data strucute which the operations are performed as:
+// LIFO ==> Last In First Out
+//There are two ways to implement a stack
+// ## Using array
+// ## Using linked list
+
+//*** Bassic Operation Of Stack
+
+// Push() => Add element To the stack  ===> O(1)
+// Add Item to the stack if not full, otherwise return
+// Pop() => to remove an element from the stack  ===> O(1)
+// return the top item if the stack not empty
+// Top() => Return top element on the stack  ===> O(1)
+// isEmpty() => Return true if the stack is empty  ===> O(1)
+//size() => returns the size of stack  ===> O(1)
